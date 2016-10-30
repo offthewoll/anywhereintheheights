d47cc9b7cbf35bd8e40271b443bcc05a7c3d7c92
Avi Wollman <aviwoll@gmail.com>
2016-10-30 16:23:50 -0400

    Added initial url and view structure

--- mealmatcher/views.py
+++ mealmatcher/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from mealmatcher.forms import *
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html', {'form':form})
+
+def guest(request):
+    return render(request, 'guest.html', {'form':guestForm})
+
+def host(request):
+    return render(request, 'host.html', {'form':host_form})
