0a498adb89a1816865e769e986715dfc18dd2189
Avi Wollman <aviwoll@gmail.com>
2016-11-06 13:19:19 -0500

    Changed coed model name and added string definitions to models

--- mealmatcher/models.py
+++ mealmatcher/models.py
@@ -3,20 +3,32 @@ from django.contrib.auth.models import User
     
 class MealTime(models.Model):
     MealType = models.CharField(max_length = 14)
+    
+    def __str__(self):
+        return self.MealType
 
 class Gender(models.Model):
     GenderType = models.CharField(max_length = 13)
+    
+    def __str__(self):
+        return self.GenderType
 
-class Coed(models.Model):
+class CoedSettings(models.Model):
     CoedType = models.CharField(max_length = 40)
+    
+    def __str__(self):
+        return self.CoedType
     
 class FoodRestriction(models.Model):
     RestrictionType = models.CharField(max_length = 13)
+    
+    def __str__(self):
+        return self.RestrictionType
 
 class UserSettings(models.Model):
     UserID = models.ForeignKey(User, on_delete=models.CASCADE)
     Gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
-    Coed = models.ForeignKey(Coed, on_delete=models.CASCADE)
+    CoedSetting = models.ForeignKey(CoedSettings, on_delete=models.CASCADE)
 
 class Host(models.Model):
     UserID = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -32,7 +44,7 @@ class Meal(models.Model):
     HostID = models.ForeignKey(User, on_delete=models.CASCADE)
     MealDate = models.DateField()
     MealTimeID = models.ForeignKey(MealTime, on_delete=models.CASCADE)
-    Coed = models.ForeignKey(Coed, on_delete=models.CASCADE)
+    CoedSetting = models.ForeignKey(CoedSettings, on_delete=models.CASCADE)
 
 class GuestRequest(models.Model):
     HostID = models.ForeignKey(User, on_delete=models.CASCADE)
