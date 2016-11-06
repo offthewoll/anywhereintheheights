f612c6b4cf3e582b802efd8d155cb322e6f3281c
Avi Wollman <aviwoll@gmail.com>
2016-11-06 13:44:50 -0500

    Added custom verbose_name_plural settings to models already plural

--- mealmatcher/models.py
+++ mealmatcher/models.py
@@ -16,6 +16,9 @@ class Gender(models.Model):
 class CoedSettings(models.Model):
     CoedType = models.CharField(max_length = 40)
     
+    class Meta:
+        verbose_name_plural = 'Coed Settings'
+    
     def __str__(self):
         return self.CoedType
     
@@ -29,6 +32,9 @@ class UserSettings(models.Model):
     UserID = models.ForeignKey(User, on_delete=models.CASCADE)
     Gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
     CoedSetting = models.ForeignKey(CoedSettings, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name_plural = 'User Settings'
 
 class Host(models.Model):
     UserID = models.ForeignKey(User, on_delete=models.CASCADE)
